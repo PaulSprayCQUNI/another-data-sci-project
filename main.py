bcb8ccd1dd1b914ea7ac260f512a7e9fb0112e21
PaulSprayCQUNI <paul.spray@cqumail.com>
2024-04-01 15:10:29 +1000

    place holder functions added

---
+++ main.py
@@ -0,0 +1,21 @@
+# main 
+import pandas as pd
+import pandas as pd
+# main 
+
+# Import the dataset
+dataset = pd.read_csv('path/to/your/dataset.csv')
+
+# Continue with your code
+def paul_sfunction():
+    print('Hello from Paul')
+    return
+
+def francis_sfunction():
+    ## what have you got to say Francis?
+    #print('Hello from Francis')
+    #return
+
+def Mat_s_function():   
+    # print('Hello from Mat')
+    # return
