3dfaf168fcddd1f4aea4d09f90de4699887f148c
PaulSprayCQUNI <paul.spray@cqumail.com>
2024-04-01 15:26:36 +1000

    functions renamed

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import pandas as pd
 dataset = pd.read_csv('path/to/your/dataset.csv')
 
 # Continue with your code
-def paul_sfunction():
+def paul_s_function():
     print('Hello from Paul')
     return
 
-def francis_sfunction():
+def francis_r_function():
     ## what have you got to say Francis?
     #print('Hello from Francis')
     #return
 
-def Mat_s_function():   
+def mathew_g_function():   
     # print('Hello from Mat')
     # return
